d018c82402b0cf559f33e79d0557e8bf1fcb8c5a
andyjrii <enehizenajames@gmail.com>
2023-12-04 23:18:35 +0100

    added global api and enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: FRONTEND_BASE_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    preflightContinue: false,
+    maxAge: 86400,
+  });
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(3300);
 }
 bootstrap();
