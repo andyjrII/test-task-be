d829d0433064c0ab6a04728713bfa824a1ed0567
andyjrii <enehizenajames@gmail.com>
2023-12-07 12:42:46 +0100

    formatted data text aligned frontend

--- src/sectors/interfaces/sector.interface.ts
+++ src/sectors/interfaces/sector.interface.ts
@@ -1,6 +1,6 @@
 export interface MySector {
-  id: number;
-  name: string;
-  parentId: number;
-  subcategories?: MySector[];
+  key: number; // Represent hierarchy level
+  text: string; // Change 'name' to 'text'
+  value: number; // Change 'id' to 'value'
+  children?: MySector[]; // Change 'subcategories' to 'children'
 }
--- src/sectors/sectors.service.ts
+++ src/sectors/sectors.service.ts
@@ -9,35 +9,42 @@ export class SectorsService {
 
   async getSectors(): Promise<MySector[]> {
     const sectors = await this.prisma.sector.findMany();
-    const example = await this.formatSectors(sectors);
-    console.log(example);
-    return example;
+    return this.formatSectors(sectors, 1); // Start hierarchy level at 1
   }
 
-  private async formatSectors(sectors: Sector[]): Promise<MySector[]> {
+  private async formatSectors(
+    sectors: Sector[],
+    hierarchy: number,
+  ): Promise<MySector[]> {
     const formattedSectors: MySector[] = [];
 
     for (const sector of sectors) {
-      const formattedSector = await this.formatSector(sector);
+      const formattedSector = await this.formatSector(sector, hierarchy);
       formattedSectors.push(formattedSector);
     }
 
     return formattedSectors;
   }
 
-  private async formatSector(sector: Sector): Promise<MySector> {
+  private async formatSector(
+    sector: Sector,
+    hierarchy: number,
+  ): Promise<MySector> {
     const subcategories = await this.prisma.sector.findMany({
       where: { parentId: sector.id },
     });
 
     const formattedSector: MySector = {
-      id: sector.id,
-      name: sector.name,
-      parentId: sector.parentId,
+      key: hierarchy, // Assign hierarchy level
+      text: sector.name, // Map 'name' to 'text'
+      value: sector.id, // Map 'id' to 'value'
     };
 
     if (subcategories.length > 0) {
-      formattedSector.subcategories = await this.formatSectors(subcategories);
+      formattedSector.children = await this.formatSectors(
+        subcategories,
+        hierarchy + 1,
+      ); // Increment hierarchy for subcategories
     }
 
     return formattedSector;
